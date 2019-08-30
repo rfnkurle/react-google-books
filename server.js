5592d9207c69cbbe6a6e62d6b82ac432cbf72ee1
robiwankenobi <rfnkurle@gmail.com>
2019-08-29 17:01:29 -0700

    Correcting Server Routing

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.Promise = Promise;
+
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/r-mern-books" , { useNewUrlParser: true });
 
 // Start the API server
